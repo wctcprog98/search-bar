d89c2740cf123a62ac5735b814100b71671ca7b9
root <chinenyediei@gmail.com>
2020-10-23 10:53:51 -0500

    loops through array and displays name if match exists

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
 document.getElementById('searchInput').addEventListener('keyup', function (event) {
     let username = event.target.value.toLowerCase();
-    console.log(username); 
-})
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+ 
+    //loop through array
+    for (let i = 0; i < allNamesDOMCollection.length; i++){
+        const currentName = allNamesDOMCollection[i].textContent.toLowerCase(); //grabs text from li and changes text to lowercase
+        console.log(currentName); 
+
+        if(currentName.includes(username)){
+            allNamesDOMCollection[i].style.display = "block";
+        }
+        else{
+            allNamesDOMCollection[i].style.display = "none";
+        }
+    }
+}); 
+
+// let container = document.querySelector('#results');
+//we can also use the includes() method in if/else
